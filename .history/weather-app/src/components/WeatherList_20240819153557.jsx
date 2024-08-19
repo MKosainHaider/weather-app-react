e01690093d94797e588061daf9d80b33317e6c05
Kosain Haider <kosainhaidr81@gmail.com.com>
2024-08-19 16:23:04 +0500

    2nd commit

--- .history/weather-app/src/components/WeatherList_20240819153557.jsx
+++ .history/weather-app/src/components/WeatherList_20240819153557.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import WeatherCard from "./WeatherCard";
 
 const API_KEY = "397a3c1468c2cbe1a8784c7b6a3ba8e8";
@@ -11,15 +12,14 @@ function WeatherList({ city }) {
     if (city) {
       const fetchWeather = async () => {
         try {
-          const response = await fetch(
+          const response = await axios.get(
             `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
           );
-          const data = await response.json();
-          const dailyData = data.list.filter((reading) =>
+          const dailyData = response.data.list.filter((reading) =>
             reading.dt_txt.includes("12:00:00")
           );
           setWeatherData(dailyData);
-          setCurrentWeather(dailyData[0]); // Highlight the current weather
+          setCurrentWeather(dailyData[0]);
         } catch (error) {
           console.error("Error fetching weather data:", error);
         }
